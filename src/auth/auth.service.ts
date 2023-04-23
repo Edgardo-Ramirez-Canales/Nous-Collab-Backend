1fd2db26407ea76a45141f7e3ed86f4190787706
Edgardo <ramirezedgardo92@gmail.com>
2023-04-23 17:38:12 -0600

    update user image

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
     if (user) {
       const isMatch = await bcrypt.compare(password, user.password);
       if (isMatch) {
-        const { password,image, ...rta } = user.toJSON();
+        const { password, ...rta } = user.toJSON();
         return rta;
       }
     }
@@ -27,7 +27,7 @@ export class AuthService {
   }
 
   generateJWT(user: User) {
-    const payload: PayloadToken = { role: user.role, sub: user.id, _id: user._id };
+    const payload: PayloadToken = { role: user.role, sub: user.id, _id: user._id, image: user.image };
     return {
       access_token: this.jwtService.sign(payload),
       user,
--- src/auth/models/token.model.ts
+++ src/auth/models/token.model.ts
@@ -2,4 +2,5 @@ export interface PayloadToken {
   role: string;
   sub: number;
   _id: string;
+  image: string;
 }
